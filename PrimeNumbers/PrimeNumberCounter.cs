6c6e94ff94f743cb6a188dacb5ee968cdab30d53
Wojciech Marek <marek.wojciech@outlook.com>
2021-05-15 18:35:20 +0200

    Init projects

---
+++ PrimeNumbers/PrimeNumberCounter.cs
@@ -0,0 +1,7 @@
+namespace PrimeNumbers
+{
+    public class PrimeNumberCounter
+    {
+        
+    }
+}
---
+++ PrimeNumbersTests/PrimeNumberCounterTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace PrimeNumbersTests
+{
+    public class PrimeNumberCounterTests
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
