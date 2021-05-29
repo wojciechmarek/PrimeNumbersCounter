07fb2b4c1caa9a6a3d1d4b333e7661555a50cf5d
Wojciech Marek <marek.wojciech@outlook.com>
2021-05-29 15:58:19 +0200

    TDD step 2, method imp, test green

--- PrimeNumbers/PrimeNumberCounter.cs
+++ PrimeNumbers/PrimeNumberCounter.cs
@@ -1,10 +1,46 @@
+using System;
+
 namespace PrimeNumbers
 {
     public class PrimeNumberCounter
     {
-        public object CountPrimeNumbers(int i, int i1)
+        public int CountPrimeNumbers(int rangeMin, int rangeMax)
         {
-            throw new System.NotImplementedException();
+            if (rangeMin > rangeMax)
+            {
+                throw new InvalidOperationException("Min value cannot ve greather than max value");
+            }
+
+            if (rangeMax < 0 || rangeMin < 0)
+            {
+                throw new InvalidOperationException("Values cannot be lower than 0");
+            }
+
+            int foundPrimeNumbers = 0;
+
+            for (int i = rangeMin; i < rangeMax; i++)
+            {
+                if (IsPrime(i))
+                {
+                    foundPrimeNumbers++;
+                }
+            }
+
+            return foundPrimeNumbers;
+            
+
+            bool IsPrime(int number)
+            {
+                if (number == 1) return false;
+
+                for (int i = 2; i < number; i++)
+                {
+                    if (number % i == 0) return false;
+                }
+                    
+                return true;
+            }
+
         }
     }
 }
